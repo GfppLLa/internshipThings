869669b80943d9e83fdec357532de91ece20865f
Eu, de paula <gfpaula.cic@uesc.br>
2024-12-17 06:32:48 -0300

    more minor updates

---
+++ taskOneVerTwo.c
@@ -0,0 +1,33 @@
+/*
+not that secure as according to some level of archteture
+
+Read 2 numbers from args;
+
+Export:
+    1-Sum of numbers
+    2- product of the first N by the square of the second; 
+    3- Suqare of the first; 
+    4- SquareRoot((Square of 1)+(square of 2))
+    5- Mod of 1
+    */
+int nPow()
+{
+    
+}
+int module()
+{
+
+
+}
+
+double squareRoot()
+{
+
+}
+
+int main(int argc, char*argv[])
+{
+    if(argc<2)return 0;
+    
+return 0;
+}
